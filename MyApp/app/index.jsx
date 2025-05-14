1a21830acd15fb5146fc08e5a3d92b9d94ae3513
likhitrajy <likhitrajy@gmail.com>
2025-05-13 23:39:40 -0400

    Fixed some code

--- MyApp/app/index.jsx
+++ MyApp/app/index.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import { View, StyleSheet, SafeAreaView } from 'react-native';
 import Header from '../components/header';
-<<<<<<< Updated upstream
 import MainScreenCard from '../components/MainScreenCard'
-=======
 import Footer from '../components/footer';
->>>>>>> Stashed changes
+
 
 const MainScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-<<<<<<< Updated upstream
       <View style = {styles.cardContainer}>
       <MainScreenCard/>
       </View>
-    </View>
-=======
-
-      {/* Main scrollable or content area */}
-      <View style={styles.content}>
-        {/* Your middle page content goes here */}
-      </View>
-
       <Footer />
     </SafeAreaView>
->>>>>>> Stashed changes
   );
 };
 
